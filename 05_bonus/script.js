cefad71745971a414ea4f8325ce450a648cf2f19
biancapcr <biancacristina.pocora@gmail.com>
2025-07-08 17:55:55 +0200

    05-bonus arrow-function

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -25,9 +25,23 @@ function greetByTime(userName){
   return `${greeting} ${userName}.`;
 }
 
-
+// 2) Arrow function
+const greetByTimeArrow = userName => {
+  const hour = new Date().getHours();
+  let greeting;
+  if (hour < 13){
+    greeting = 'Buongiorno';
+} 
+  else if (hour < 17){
+    greeting = 'Buon pomeriggio';
+} 
+  else{
+    greeting = 'Buonasera';
+}
+  return `${greeting} ${userName}.`;
+};
 // Invoca la funzione qui e stampa il risultato in console
 console.log(greetByTime(name));
-
+console.log(greetByTimeArrow(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
