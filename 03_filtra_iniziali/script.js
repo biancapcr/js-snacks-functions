f00f900874d8c8a1869b0817d1945881cf30b68e
biancapcr <biancacristina.pocora@gmail.com>
2025-07-08 17:22:54 +0200

    [add] 03-filtra-iniziali arrow-function

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,13 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-// 1) Funzione tradizionale
+ // 1) Funzione tradizionale
 function filterByLetter(arr, letter){
   return arr.filter(function(word){ return word.startsWith(letter); });
 }
+ // 2) Arrow function
+const filterByLetterArrow = (arr, letter) => 
+  arr.filter(word => word.startsWith(letter));
 
 // Invoca la funzione qui e stampa il risultato in console
 console.log(filterByLetter(names, 'A'));
-
+console.log(filterByLetterArrow(names, 'A'));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
