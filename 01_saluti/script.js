2a10b85bcc47e81bbec3b4957b3705a1e3d7c251
biancapcr <biancacristina.pocora@gmail.com>
2025-07-08 16:44:40 +0200

    [add] 01-saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,16 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+// 1) Funzione tradizionale
+function hello(name){
+  return `Ciao ${name}`;
+}
 
+// 2) Arrow function
+const helloArrow = name => `Ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(hello(userName));   
+console.log(helloArrow(userName)); 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
