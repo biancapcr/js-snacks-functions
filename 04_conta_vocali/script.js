12c83a58a78005793fd10af90425db9840b5be50
biancapcr <biancacristina.pocora@gmail.com>
2025-07-08 17:29:18 +0200

    [add] 04-conta-vocali funzione-tradizionale

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+// 1) Funzione tradizionale
+function countVowels(str){
+  let count = 0;
+  const vowels = 'aeiouAEIOU';
+  for (let letter of str){
+    if (vowels.includes(letter)){
+      count++;
+    }
+}
+  return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
